763c52e3f19647178be6283640ae5a60f9c9d5a0
Pat Christopher <patrick.christopher@mtnsat.com>
2013-09-27 10:01:51 -0700

    capture_datad.py works now
    added building the netflow package into setup.sh so it would work
    in the virtual env
    forgot to add pika, added it

--- RabbitMq/setup.sh
+++ RabbitMq/setup.sh
@@ -1,6 +1,8 @@
 #  setup script for this project
 #  will create a virtual env based on the given requirements.pip file
 
+current_dir=`pwd`
+
 has_virtenv=`which virtualenv 2>&1 > /dev/null`
 if [[ $has_virtenv -ne 0 ]]; then
   apt-get install python-virtualenv
@@ -10,3 +12,19 @@ fi
 virtualenv DEV
 
 pip install -E DEV -r requirements.txt
+
+#  flowd bindings are not available in pip
+#   download flowd, build the C then install
+#   with the virutalenv python
+sudo apt-get install python-dev
+mkdir tmp
+wget https://flowd.googlecode.com/files/flowd-0.9.1.tar.gz -O tmp/flowd-0.9.1.tar.gz
+cd tmp
+tar -zxf flowd-0.9.1.tar.gz
+cd flowd-0.9.1
+./configure
+make
+sudo make install
+
+$current_dir/DEV/bin/python setup.py install
+
